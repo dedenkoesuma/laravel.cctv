0814f1d446b850f421451160b5e59d9131a4c2b9
DedenKoesuma <dedenkoesuma@DedenKoesumas-MacBook-Pro.local>
2026-05-01 19:24:26 +0700

    Update layout simple

--- app/Http/Controllers/Admin/SalesOrderController.php
+++ app/Http/Controllers/Admin/SalesOrderController.php
@@ -470,6 +470,7 @@ class SalesOrderController extends Controller
 
         } catch (\Exception $e) {
             DB::rollBack();
+            dd('TERNYATA INI ERRORNYA BANG: ' . $e->getMessage(), 'Terjadi di baris: ' . $e->getLine());
             return back()->with('error', 'Gagal membuat Invoice: ' . $e->getMessage());
         }
     }
